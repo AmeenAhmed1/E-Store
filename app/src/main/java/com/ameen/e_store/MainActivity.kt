d7a1a510e40dd54fefa2efaf7d87dfacb4dadd69
Ameen <ameenahmed386@gmail.com>
2021-05-04 00:49:52 +0200

    init commit

---
+++ app/src/main/java/com/ameen/e_store/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ameen.e_store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
